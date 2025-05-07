b9d970252d50a65aceccff57f055a362e4eb8619
Sneha <snehabopche1@gmail.com>
2025-05-07 11:52:07 -0400

    Update server.js-1

--- server.js
+++ server.js
@@ -1,19 +1,5 @@
 const express = require('express');
-
 const app = express();
-const port = 3000;
+app.get('/', (req, res) => res.send('Hello from Sneha!'));
+app.listen(3000, () => console.log('App running on port 3000'));
 
-const todos = ['This is a task', 'This is another task', 'Again another task'];
-
-app.get('/todos', (_req, res) =>
-  res.status(200).json({
-    data: todos,
-    error: null,
-  })
-);
-
-const server = app.listen(port, () =>
-  console.log(`Example app listening at http://localhost:${port}`)
-);
-
-module.exports = server;
